140be8f9c39b8cc7e2716ff865a7cb2bfaf09927
moshed <mdavidesko@gmail.com>
2018-08-09 13:42:49 +0300

    change data

--- api/controllers/videosController.js
+++ api/controllers/videosController.js
@@ -6,7 +6,7 @@ const CLOUDBINARY_API_ENDPOINT = 'https://211811477677314:-Ef3mMlkcGE3tE40t-FOX4
 exports.list_all_videos = function(req, res) {
     axios.get(CLOUDBINARY_API_ENDPOINT)
     .then(response => {
-        res.json(response.data.resources)
+        res.json(response.data)
     }).catch(function (error) {
         console.error(error);
         res.send(error)
