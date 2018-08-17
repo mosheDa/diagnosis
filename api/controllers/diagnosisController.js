dec6c6fbaac0167ff2c0c71cd5832fbf6f6c0fdc
moshed <mdavidesko@gmail.com>
2018-08-17 12:06:55 +0300

    get user data

--- api/controllers/diagnosisController.js
+++ api/controllers/diagnosisController.js
@@ -1,4 +1,6 @@
 'use strict';
+const axios = require('axios');
+const async = require ('async');
 
 var mongoose = require('mongoose'),
 Diagnosis = mongoose.model('Diagnosis');
@@ -12,11 +14,34 @@ exports.list_all_diagnosis = function(req, res) {
 };
 
 exports.list_all_users = function(req, res) {
-  Diagnosis.find({}, function(err, diagnosis) {
-    if (err)
-      res.send(err);    
-    res.send(diagnosis.map(d=> d.userName));
-  });
+  async.series({
+    usersVideos: getVideos,
+    diagnosis: function(callback){
+      Diagnosis.find({}).lean().exec(function (err, docs) {
+        if(err) return callback(err);
+        return callback(null, docs);
+        // docs are plain javascript objects instead of model instances
+    });
+    }
+}, function(err, results) {
+    const richDiagnosis = results.diagnosis.map(diagnosis=>{
+      const userVideos = results.usersVideos.filter((video) => {
+        return video.context.custom.username == diagnosis.userName;
+      })
+      console.log(diagnosis.userName, userVideos)
+      const userVideoImage = userVideos[0].public_id;
+      const videosCount = userVideos.length;
+
+      return {...diagnosis, videosCount, userVideoImage}
+    })
+
+    return res.send(richDiagnosis);
+});
+  // Diagnosis.find({}, function(err, diagnosis) {
+  //   if (err)
+  //     res.send(err);    
+  //   res.send(diagnosis.map(d=> d.userName));
+  // });
 };
 
 exports.create_a_diagnosis = function(req, res) {
@@ -64,3 +89,14 @@ exports.delete_a_diagnosis = function(req, res) {
     res.json({ message: 'Diagnosis successfully deleted' });
   });
 };
+
+const getVideos = (callback) => {
+  const CLOUDBINARY_API_ENDPOINT = 'https://211811477677314:-Ef3mMlkcGE3tE40t-FOX4LVqbw@api.cloudinary.com/v1_1/dtvoiy5lg/resources/video/context/?max_results=500&key=username';  
+
+  axios.get(CLOUDBINARY_API_ENDPOINT)
+  .then(res => {
+    return callback(null, res.data.resources);
+  }).catch(err =>{
+    return callback(err)
+  });
+}
